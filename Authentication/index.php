26ab630fb76b75dfacefb347895d41bd08e245c6
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-03-22 18:29:18 +0530

    First Commit

---
+++ Authentication/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Register.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contanier__Register_Background">
+        <div class="Register_Form">
+           <img src="/Assets/motorcycle.gif" />
+        </div>
+        <div class="Register_Form_view" >
+            <h3>Register</h3>
+            <div class="input_Area">
+                <label for=""><b>Username</b></label>
+            <input type="text" id="#"  class="input_box"/>
+            </div>
+            <div class="input_Area">
+            <label class="label"><b>Password</b></label>
+            <input type="text" id="#" class="input_box" />
+            </div>
+            <div class="input_Area">
+            <label class="label"><b>Password</b></label>
+            <input type="text" id="#" class="input_box" />
+            </div>
+            <div class="input_Area labels">
+                <div><label class="label2">Already Register? </label></div>
+                <div><label><a href="#" class="link"> Login</a></label></div>
+            </div>
+            <div class="input_Area buttons">
+            <input type="button" value="Register" id="#" class="btn"/>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
